b5464fd76b658f9912a81f6a959234da2f590383
znbdev <znbdev@outlook.com>
2024-09-28 09:53:49 +0900

    Modify

--- src/test/java/com/example/Java8DemoTest.java
+++ src/test/java/com/example/Java8DemoTest.java
@@ -1,6 +1,10 @@
 package com.example;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class Java8DemoTest {
 
@@ -12,5 +16,11 @@ class Java8DemoTest {
     @Test
     void userValidatorTest() {
         Java8Demo.userValidator();
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void testWithMultipleValues(int value) {
+        assertEquals(value, value);
     }
 }
